03d484e801c7f52b614fa9c73125091716592393
Des Grainger <des.grainger@proton.me>
2023-04-24 19:42:00 +0100

    Added delete functionality.

--- app.py
+++ app.py
@@ -38,5 +38,28 @@ def index():
         return render_template("index.html", tasks=tasks)
 
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was a problem deleting that task'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('update.html', task=task_to_update)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
